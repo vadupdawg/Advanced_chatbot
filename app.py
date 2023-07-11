a627c38cb75fb6c5b47f5a983d9b6c6029b37bd7
vadupdawg <niels1214@gmail.com>
2023-07-11 16:04:50 +0200

    advanced

--- app.py
+++ app.py
@@ -7,16 +7,18 @@ import pinecone
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.chains import RetrievalQA
+from langchain.agents import Tool
+from langchain.agents import initialize_agent
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 pinecone.init(
-    api_key = os.getenv("PINECONE_API_KEY"),  # find at app.pinecone.io
-    environment = os.getenv("PINECONE_ENV"),  # next to api key in console
+    api_key = os.getenv("PINECONE_API_KEY"),
+    environment = os.getenv("PINECONE_ENV"),
 )
-index_name = "groeimetai-advanced"
+index_name = "groeimetai"
 
 nest_asyncio.apply()
 
@@ -30,31 +32,67 @@ if root.handlers:
         root.removeHandler(handler)
 logging.basicConfig(level=logging.WARNING, stream=sys.stdout)
 
-chat_histories = {}
-
-# create a memory object
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-
-# Adding initial chat history
-initial_message = "Hi ik ben de chatbot van GroeimetAi. GroeimetAi is een bedrijf dat middel en klein bedrijf helpt met het implementeren van AI mogelijkheden en gebruik te maken van deze geweldige trend. Ik kan je helpen met al je vragen over AI implementatie, ik ben echter alleen getrained op de data die OpenAI mij heeft gevoerd tot September 2021."
-memory.chat_memory.messages.append((memory.ai_prefix, initial_message))
-
 #create an embeddings object
 embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
 
 #create a vectorstore object
 vectorstore = Pinecone.from_existing_index(index_name, embeddings, namespace="docs")
 
-global qa
-qa = ConversationalRetrievalChain.from_llm(
-    ChatOpenAI(
-        temperature=0, 
-        model="gpt-3.5-turbo-16k", 
-        openai_api_key=openai_api_key), 
-        vectorstore.as_retriever(),
-        condense_question_llm = ChatOpenAI(temperature=0, model='gpt-3.5-turbo', openai_api_key=openai_api_key, verbose=True), 
-        memory=memory,
-        verbose=True)
+# chat completion llm
+llm = ChatOpenAI(
+    openai_api_key=openai_api_key,
+    model_name="gpt-4",
+    temperature=0.0
+)
+
+# conversational memory
+conversational_memory = ConversationBufferWindowMemory(
+    memory_key='chat_history',
+    k=5,
+    return_messages=True,
+)
+conversational_memory.save_context(
+    {
+        "input": "Jij bent de GroeimetAi Chatbot, jouw partner voor AI-gedreven chatbots. We bieden vijf service levels: Personal, Advanced, Professional, Startup en Enterprise, elk met hun eigen premium features. Onze chatbots zijn aanpasbaar, meertalig en kunnen worden geïntegreerd met jouw website, WhatsApp en CRM-systemen. Jij bent de Advanced chatbot en hebt ook alle informatie die nodig is om de klant te informeren over de verschillende service levels."
+    },
+    {
+        "output": "Begrepen! Ik zal informatie verstrekken over onze chatbot-oplossingen, de verschillende service levels en hoe ze jouw bedrijf kunnen helpen. Voor complexe vragen zal ik voorstellen om een gesprek te starten, maar zal altijd zelf proberen de informatie te vinden die nodig is om de klant te helpen. Laat me weten over welk soort bedrijf je informatie wilt, zodat ik relevante voorbeelden kan geven."
+    },
+)
+
+
+# retrieval qa chain
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectorstore.as_retriever()
+)
+
+tools = [
+    Tool(
+        name='Product Knowledge Base',
+        func=qa.run,  # Dit zou een RetrievalQA instantie zijn voor productgerelateerde vragen
+        description='use this tool when answering questions about GroeimetAi products.'
+    ),
+    Tool(
+        name='General Knowledge Base',
+        func=qa.run,  # Dit zou een RetrievalQA instantie zijn voor algemene vragen
+        description='use this tool when answering general questions about GroeimetAi.'
+    )
+]
+
+agent = initialize_agent(
+    agent='chat-conversational-react-description',
+    tools=tools,
+    llm=llm,
+    verbose=True,
+    handle_parsing_errors="Check the latest user question and try answering it again in the language of the user.",
+    max_iterations=3,
+    early_stopping_method='generate',
+    memory=conversational_memory
+)
+
+
 
 @app.route("/")
 def index():
@@ -65,10 +103,12 @@ def chat():
     data = request.get_json()
     query = data['user_message']
 
-    result = qa({"question": query})
+    # Call the agent with the user's query
+    result = agent(query)
 
-    return jsonify(chatbot_response=result["answer"])
+    # Return the agent's response
+    return jsonify(chatbot_response=result["output"])
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5001, debug=True)
