8bb789174f1e5b2feb7fe8d13fb7239d035b839a
vadupdawg <niels1214@gmail.com>
2023-07-21 14:52:33 +0200

    qdrant database

--- app.py
+++ app.py
@@ -3,27 +3,26 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask import Flask, render_template, request, jsonify
 import logging
+from langchain import LLMChain
 import nest_asyncio
 import sys
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Weaviate
+from langchain.vectorstores import Qdrant
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.chains import RetrievalQA
-from langchain.agents import Tool
-from langchain.agents import initialize_agent
+from langchain.prompts import BaseChatPromptTemplate
+from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent, AgentOutputParser
+from langchain.schema import AgentAction, AgentFinish, HumanMessage
 from langchain.callbacks import get_openai_callback
+from typing import List, Union
+import re
+from qdrant_client import QdrantClient
 import weaviate
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
-weaviate_api_key = os.getenv("WEAVIATE_API_KEY")
-weaviate_url = os.getenv("WEAVIATE_URL")
-
-client = weaviate.Client(
-                    url=weaviate_url,
-                    auth_client_secret=weaviate.AuthApiKey(api_key=weaviate_api_key),
-                    additional_headers={"X-OpenAI-Api-Key": os.environ.get('OPENAI_API_KEY')},
-    )
+qdrant_api_key = os.getenv("QDRANT_API_KEY")
+qdrant_url = os.getenv("QDRANT_URL")
 
 nest_asyncio.apply()
 
@@ -46,8 +45,10 @@ logging.basicConfig(level=logging.WARNING, stream=sys.stdout)
 #create an embeddings object
 embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
 
-#create a vectorstore object
-vectorstore = Weaviate(client, "GroeimetAi", "text", embedding=embeddings)
+# create a vectorstore object
+client = QdrantClient(url=qdrant_url, api_key=qdrant_api_key)
+collection_name = "groeimetai4"
+qdrant = Qdrant(client, collection_name, embeddings=embeddings)
 
 # chat completion llm
 llm = ChatOpenAI(
@@ -76,38 +77,108 @@ conversational_memory.save_context(
 qa = RetrievalQA.from_chain_type(
     llm=llm,
     chain_type="stuff",
-    retriever=vectorstore.as_retriever()
+    retriever=qdrant.as_retriever()
 )
 
 tools = [
     Tool(
-        name='Product Kennis Bank',
+        name='Similarity search database',
         func=qa.run,  # Dit zou een RetrievalQA instantie zijn voor productgerelateerde vragen
-        description='gebruik deze tool bij het beantwoorden van vragen over GroeimetAi-producten.'
-    ),
-    Tool(
-        name='Generiek Kennis Bank',
-        func=qa.run,  # Dit zou een RetrievalQA instantie zijn voor algemene vragen
-        description='gebruik deze tool bij het beantwoorden van algemene vragen over GroeimetAi.'
-    ),
-    Tool(
-        name='Prijzen Kennis Bank',
-        func=qa.run,  # Dit zou een RetrievalQA instantie zijn voor prijsgerelateerde vragen
-        description='gebruik deze tool bij het beantwoorden van vragen specifiek over het prijsschema van GroeimetAi.'
+        description='Gebruik deze tool om de database te doorzoeken naar de informatie waar de gebruiker naar op zoek is.'
     )
 ]
 
-agent = initialize_agent(
-    agent='chat-conversational-react-description',
+template = """Complete the objective as best you can. You have access to the following tools:
+
+{tools}
+
+Use the following format:
+  
+Question: the input question you must answer or a simple greeting
+Thought: je moet altijd nadenken over wat te doen wanneer je een vraag krijgt
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: het resultaat van de action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: het finale antwoord op de input question
+
+These were previous tasks you completed:
+
+
+
+Begin!
+
+Question: {input}
+{agent_scratchpad}"""
+
+# Set up a prompt template
+class CustomPromptTemplate(BaseChatPromptTemplate):
+    # The template to use
+    template: str
+    # The list of tools available
+    tools: List[Tool]
+    
+    def format_messages(self, **kwargs) -> str:
+        # Get the intermediate steps (AgentAction, Observation tuples)
+        # Format them in a particular way
+        intermediate_steps = kwargs.pop("intermediate_steps")
+        thoughts = ""
+        for action, observation in intermediate_steps:
+            thoughts += action.log
+            thoughts += f"\nObservation: {observation}\nThought: "
+        # Set the agent_scratchpad variable to that value
+        kwargs["agent_scratchpad"] = thoughts
+        # Create a tools variable from the list of tools provided
+        kwargs["tools"] = "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools])
+        # Create a list of tool names for the tools provided
+        kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
+        formatted = self.template.format(**kwargs)
+        return [HumanMessage(content=formatted)]
+
+prompt = CustomPromptTemplate(
+    template=template,
     tools=tools,
-    llm=llm,
-    verbose=True,
-    handle_parsing_errors="Check het laatste bericht van de gebruiker en probeer het opnieuw, in zijn taal.",
-    max_iterations=3,
-    early_stopping_method='generate',
-    memory=conversational_memory,
-    agent_kwargs={ "Welke taal moet ik aanhouden?" : "over het algemeen nederlands, behalve als anders wordt gecommuniceerd", "Wat als ik niet meer informatie heb?" : "Probeer dan door te vragen, en nooit geen andere professionals aan te raden."}
+    # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
+    # This includes the `intermediate_steps` variable because that is needed
+    input_variables=["input", "intermediate_steps"]
 )
+
+class CustomOutputParser(AgentOutputParser):
+    
+    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
+        # Check if agent should finish
+        if "Final Answer:" in llm_output:
+            return AgentFinish(
+                # Return values is generally always a dictionary with a single `output` key
+                # It is not recommended to try anything else at the moment :)
+                return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
+                log=llm_output,
+            )
+        # Parse out the action and action input
+        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
+        match = re.search(regex, llm_output, re.DOTALL)
+        if not match:
+            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
+        action = match.group(1).strip()
+        action_input = match.group(2)
+        # Return the action and action input
+        return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
+    
+    
+output_parser = CustomOutputParser()
+# LLM chain consisting of the LLM and a prompt
+llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+tool_names = [tool.name for tool in tools]
+agent = LLMSingleActionAgent(
+    llm_chain=llm_chain, 
+    output_parser=output_parser,
+    stop=["\nObservation:"],
+    allowed_tools=tool_names
+)
+
+agent = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
 
 @app.route("/")
 def index():
